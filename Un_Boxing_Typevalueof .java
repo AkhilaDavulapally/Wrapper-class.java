5bd954c96ec219ab0473563e130b0dba040adc07
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-08 17:57:54 +0530

    Create Un_Boxing_Typevalueof .java

---
+++ Un_Boxing_Typevalueof .java
@@ -0,0 +1,11 @@
+package com.Wrapper_classes;
+public class Un_Boxing_Typevalueof {
+	public static void main(String[] args) {
+		Integer i=10;
+		int a=i.intValue();
+		Double d=2.0;
+		double d1=d.doubleValue();
+		System.out.println(i+"->premitive"+a);
+		System.out.println(d+"->premitive"+d1);
+	}
+}
