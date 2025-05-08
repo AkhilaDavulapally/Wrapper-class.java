0307b95f83c81ace736ef57a4209c229f56c7757
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-08 17:58:27 +0530

    Create Un_Boxing.java

---
+++ Un_Boxing.java
@@ -0,0 +1,11 @@
+package com.Wrapper_classes;
+public class Un_Boxing {
+	public static void main(String[] args) {
+		Integer i=10;
+		int a=i;
+		Double d=2.0;
+		double d1=d;
+		System.out.println(i+"->premitive"+a);
+		System.out.println(d+"->premitive"+d1);
+	}
+}
